38e6cf5e3a5e92c7b9b0b978be9282adfdcfd99d
makki0205 <llxo2_5oxll@icloud.com>
2018-02-15 14:06:03 +0900

    .

---
+++ controller/app.go
@@ -0,0 +1,20 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	uuid "github.com/satori/go.uuid"
+)
+
+var uid = ""
+
+func SetToken(c *gin.Context) {
+	token := c.Query("token")
+	if token != "BAC92613-0C14-4F78-9C1E-93D431AF6870" {
+		c.JSON(400, "token err")
+		return
+	}
+	uid = uuid.NewV4().String()
+	c.JSON(200, gin.H{
+		"uuid": uid,
+	})
+}
